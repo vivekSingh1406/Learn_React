5631fdcec62b0ab9bd2ea92912b208bec0cb719e
vivek-singh <viveksb1207@gmail.com>
2026-01-11 01:09:20 +0530

    remove backend file

--- backend/src/main/java/com/example/backend/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/example/backend/repository/CourseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> { }
